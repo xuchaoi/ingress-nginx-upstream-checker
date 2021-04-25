df6773eba489b62b3e2b809f516522bb6478f7ca
xuchaoi <1141954686@qq.com>
2021-04-25 15:32:36 +0800

    Update: dockerfile (add bad backends json path)

--- build/image.sh
+++ build/image.sh
@@ -4,5 +4,5 @@ rm -rf upstream-checker
 mv ../cmd/upstream-checker/upstream-checker .
 chmod +x upstream-checker
 
-docker build --no-cache -t xuchaoi/upstream-checker:v0.5 .
+docker build --no-cache -t xuchaoi/upstream-checker:v0.6 .
 rm -rf upstream-checker
